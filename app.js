e2f6b656b92f1eca05dc40554573a7ec2696e291
Emtiaz2000 <62500633+Emtiaz2000@users.noreply.github.com>
2020-12-22 19:47:09 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const  github = new Github()
 const profilel_show = document.querySelector("#profilel_show")
 const search = document.querySelector('#search');
 const submit = document.querySelector('#button')
-
+//working after click the button 
 submit.addEventListener('click',(e)=>{
 e.preventDefault();
 if(search === null){
+    //if input is null
     alert('please fill the input')
 }else{
-    
+    //after get the data from input
      github.fetchData(search.value).then((text)=>{
         const ui = new UI();
         ui.display(text);
